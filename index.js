5727b38138b4d720abb6fd0cf04f176bda9de4b6
siam-khan-alt <nssiam99@gmail.com>
2025-12-07 19:50:31 +0800

    Integrate Firebase Admin & Role API Setup Firebase Admin SDK, implemented erifyToken middleware, and added protected API routes (/users/register, /users/role) for user role management in MongoDB.

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const admin = require('firebase-admin');
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors(
+  {
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
+));
 app.use(express.json());
 require('dotenv').config();
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString(
+  'utf-8'
+)
+const serviceAccount = JSON.parse(decoded)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+const verifyToken = async (req, res, next) => {
+    const token = req?.headers?.authorization?.split(' ')[1]
+    console.log(token)
+    if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
+    try {
+      const decoded = await admin.auth().verifyIdToken(token)
+      req.tokenEmail = decoded.email
+      next()
+    } catch (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'Unauthorized Access!', err })
+    }
+}
 const uri = `${process.env.MONGODB_URI}`
 const client = new MongoClient(uri, {
   serverApi: {
@@ -18,16 +48,63 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const database = client.db("ClubSphereDB"); 
+    const usersCollection = database.collection("users");
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+    app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+
+    app.post('/users/register', async (req, res) => {
+        try {
+            const { name, email, photoURL } = req.body;
+            
+            const existingUser = await usersCollection.findOne({ email });
+            if (existingUser) {
+                return res.status(200).json({ message: 'User already exists in DB', role: existingUser.role });
+            }
+
+            const newUser = { 
+                name, 
+                email, 
+                photoURL, 
+                role: 'member',
+                createdAt: new Date()
+            };
+
+            await usersCollection.insertOne(newUser);
+            res.status(201).json({ message: 'User registered in DB successfully', role: 'member' });
+        } catch (error) {
+            console.error('DB registration error:', error);
+            res.status(500).json({ message: 'Failed to register user in DB', error: error.message });
+        }
+    });
+
+    app.get('/users/role', verifyToken, async (req, res) => {
+        try {
+            const email = req.tokenEmail; 
+            
+            const user = await usersCollection.findOne({ email }, { projection: { role: 1 } }); 
+
+            if (!user) {
+                return res.status(404).json({ message: 'User not found in database.' });
+            }
+
+            res.json({ role: user.role });
+
+        } catch (error) {
+            console.error('Role fetch error:', error);
+            res.status(500).json({ message: 'Failed to fetch user role', error: error.message });
+        }
+    });
     app.get('/', (req, res) => {
     res.send('Hello World!')
     })
 
-    app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    
    })
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
---
+++ serviceKeyConverter.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+const jsonData = fs.readFileSync('./serviceKeyFirebase.json')
+
+const base64String = Buffer.from(jsonData, 'utf-8').toString('base64')
+console.log(base64String)
